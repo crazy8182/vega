0fe6d5f60b1c3a70aeddf36ea5637759e9244f4f
Mahesh8188 <ddbambhava8182@gmail.com>
2024-12-24 08:27:00 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [@Infinity_XBotz](https://t.me/Infinity_XBotz)**"
+        cap = "**BY›› [@vegamoviesnewin](https://t.me/vegamoviesnewin)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
