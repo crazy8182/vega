89fe0274dfb0035f1c93ad9feed5f0111e8f9ace
crazy8182 <crazygaming4694@gmail.com>
2025-01-01 16:38:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb://admin:admin@localhost:27017/admin")
 DATABASE_NAME = environ.get('DATABASE_NAME', "admin")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'lazyfilesx')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
